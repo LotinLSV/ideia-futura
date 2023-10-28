faa8ff1acb3567a7e4d9d5a9cd53de3dd863f310
lotinlkp <lucasdesouzavaz@hotmail.com>
2023-10-28 11:34:22 -0300

    Adicionando relatório

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
    return ( 
     <div width="100%" height="100%">
    <h1 text-alling="center">Relatório Mundo de projetos</h1>
-   <iframe src="https://app.powerbi.com/view?r=eyJrIjoiNmYwOGUyZjAtMDU5Yy00ODIxLTlmZTgtYmFkYjRkZDM4OGM5IiwidCI6IjZjMDAzNjVlLTRhY2UtNDVhNi05NDA5LWIzOTI4OTc1Yzc3ZSJ9"></iframe>
+   <iframe width="1920" height="1080" src="https://app.powerbi.com/view?r=eyJrIjoiNmYwOGUyZjAtMDU5Yy00ODIxLTlmZTgtYmFkYjRkZDM4OGM5IiwidCI6IjZjMDAzNjVlLTRhY2UtNDVhNi05NDA5LWIzOTI4OTc1Yzc3ZSJ9"></iframe>
    </div>
    )
 }
