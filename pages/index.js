e6aa956487f5e689f0e54751d566a2db63d2873e
lotinlkp <lucasdesouzavaz@hotmail.com>
2023-10-28 11:25:36 -0300

    Adicionando relatório

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,10 @@
 function Home() {
- // index.js
-document.addEventListener("DOMContentLoaded", function () {
-    const container = document.getElementById("container");
-    fetch("index.html")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Não foi possível carregar a página HTML.");
-            }
-            return response.text();
-        })
-        .then(htmlContent => {
-            container.innerHTML = htmlContent;
-        })
-        .catch(error => {
-            console.error(error);
-        });
-});
-
+   return ( 
+    <div width="100%" height="100%">
+   <h1 text-alling="center">Relatório Mundo de projetos</h1>
+   <iframe title="Report Section" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiNmYwOGUyZjAtMDU5Yy00ODIxLTlmZTgtYmFkYjRkZDM4OGM5IiwidCI6IjZjMDAzNjVlLTRhY2UtNDVhNi05NDA5LWIzOTI4OTc1Yzc3ZSJ9" frameborder="0" allowFullScreen="true"></iframe>
+   </div>
+   )
 }
 
 export default Home
