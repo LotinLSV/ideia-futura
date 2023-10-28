ed2a4842f9d3ba782cc64b9bd0c64e0fb1702ad2
lotinlkp <lucasdesouzavaz@hotmail.com>
2023-10-28 10:42:54 -0300

    primeira página

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ function Home() {
     <h3>Home</h3>
    <p>este é um novo parágrafo</p>
    <p>este é o segundo paragrafo</p>
+   <iframe title="Report Section" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiNmYwOGUyZjAtMDU5Yy00ODIxLTlmZTgtYmFkYjRkZDM4OGM5IiwidCI6IjZjMDAzNjVlLTRhY2UtNDVhNi05NDA5LWIzOTI4OTc1Yzc3ZSJ9" frameborder="0" allowFullScreen="true"></iframe>
    </div>) 
 
 }
