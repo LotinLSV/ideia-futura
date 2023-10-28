ffcf0a686640f615f0c4f569fe9c368afd61c16a
lotinlkp <lucasdesouzavaz@hotmail.com>
2023-10-28 11:17:14 -0300

    Adicionando relatório

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
 function Home() {
- window.location.href  = "/index.html" 
+ // index.js
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.getElementById("container");
+    fetch("index.html")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Não foi possível carregar a página HTML.");
+            }
+            return response.text();
+        })
+        .then(htmlContent => {
+            container.innerHTML = htmlContent;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
+
 }
 
 export default Home
